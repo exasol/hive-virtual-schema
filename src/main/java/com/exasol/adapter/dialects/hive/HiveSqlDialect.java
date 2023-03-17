8a7cdd9dfb881efa61b649edbe48d9e0b5115eaf
KK <christoph.kuhnke@exasol.com>
2023-03-17 15:44:33 +0100

    Renamed error codes

--- src/main/java/com/exasol/adapter/dialects/hive/HiveSqlDialect.java
+++ src/main/java/com/exasol/adapter/dialects/hive/HiveSqlDialect.java
@@ -143,7 +143,7 @@ public class HiveSqlDialect extends AbstractSqlDialect {
         try {
             return new HiveMetadataReader(this.connectionFactory.getConnection(), this.properties);
         } catch (final SQLException exception) {
-            throw new RemoteMetadataReaderException(ExaError.messageBuilder("E-VS-HIVE-1")
+            throw new RemoteMetadataReaderException(ExaError.messageBuilder("E-VSHIVE-1")
                     .message("Unable to create Hive remote metadata reader. Caused by: {{cause|u}}",
                             exception.getMessage()) //
                     .toString(), exception);
