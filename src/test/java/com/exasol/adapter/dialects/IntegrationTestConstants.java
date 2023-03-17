ce044736ccff38078754d148089c846979a1541f
KK <christoph.kuhnke@exasol.com>
2023-03-17 15:10:13 +0100

    Updated artifact references

--- src/test/java/com/exasol/adapter/dialects/IntegrationTestConstants.java
+++ src/test/java/com/exasol/adapter/dialects/IntegrationTestConstants.java
@@ -3,7 +3,7 @@ package com.exasol.adapter.dialects;
 import java.nio.file.Path;
 
 public final class IntegrationTestConstants {
-    public static final String VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION = "virtual-schema-dist-10.1.0-hive-2.0.2.jar";
+    public static final String VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION = "virtual-schema-dist-10.5.0-hive-2.0.3.jar";
     public static final String EXASOL_DOCKER_IMAGE_REFERENCE = "7.1.13";
     public static final Path PATH_TO_VIRTUAL_SCHEMAS_JAR = Path.of("target", VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION);
     public static final String SCHEMA_EXASOL = "SCHEMA_EXASOL";
